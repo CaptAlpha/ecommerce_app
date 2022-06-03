73ad7243b7d0bebeb01650ee63201af29ec30d7a
CaptAlpha <arhitbose.tagore2020@vitstudent.ac.in>
2022-06-03 21:49:38 +0530

    tabnine

--- lib/main.dart
+++ lib/main.dart
@@ -8,5 +8,61 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Zero to Unicorn',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(),
+      bottomNavigationBar: BottomAppBar(),
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  const CustomAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Container(
+        color: Colors.black,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Text(
+          'Zero to Unicorn',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontFamily: 'Avenir',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      iconTheme: IconThemeData(
+        color: Colors.black,
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
 }
